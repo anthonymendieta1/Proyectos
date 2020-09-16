657959507cca6bb38e094d32414b51f163ca1df7
Jonathan1010 <51387003+Jonathan1010@users.noreply.github.com>
2020-09-16 15:06:02 -0500

    Proyecto App Web Node

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(body_paser.json());
 app.use(body_paser.urlencoded({extended:true}));
 app.use('/' , rutas);
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000
 app.listen(port,function(){
     console.log("El servidor se inicio correctamente")
 });
